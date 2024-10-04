96e6a7a7d99ebd9f9218b803bece03c8777a17e9
amschel <102370012+amschel99@users.noreply.github.com>
2024-10-04 15:55:22 +0300

    Update Index.sol

--- src/Index.sol
+++ src/Index.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./School.sol";
+import "./School1.sol";
 
 contract Factory is School {
+      struct StudentStruct{
+        uint256 id;
+string name;
+address school;
+
+
+    }
+
     event SchoolDeployed(address indexed schoolAddress);
     mapping(address=>address) public deployedContracts;
+    mapping(uint256=>StudentStruct) public StudentData;
+    mapping (address=>uint256) public Students;
+ 
+
+
+
+
    
     constructor(string memory _schoolName, SchoolType _schoolType, SchoolCategory _schoolCategory, uint256 _id) 
         School(_schoolName, _schoolType, _schoolCategory, _id) 
@@ -16,14 +31,25 @@ contract Factory is School {
         deployedContracts[msg.sender]= address(newSchool);
     }
 
-    // function deploySchoolContract(
-    //     string memory _schoolName,
-    //     SchoolType _schoolType,
-    //     SchoolCategory _schoolCategory,
-    //     uint256 _id
-    // ) public returns (address) {
-    //     School newSchool = new School(_schoolName, _schoolType, _schoolCategory, _id);
-    //     emit SchoolDeployed(address(newSchool));
-    //     return address(newSchool);
-    // }
+    function registerStudent(string memory _name, address payable _school, uint256 _id) public returns (string memory){
+        require(StudentData[_id].id != 0, "Student with that ID is already registered!");
+StudentStruct memory newStudent= StudentStruct({
+    id:_id,
+    name:_name,
+    school:_school
+});
+StudentData[_id]=newStudent;
+Students[msg.sender]=_id;
+return "Successs";
+
+
+    }
+    function payFees(uint256 _studentId, address payable  _to )public payable {
+        require(!Students[msg.sender], "You are not registered on the platform yet");
+        
+         _to.transfer(msg.value);
+
+    }
+
+ 
 }
